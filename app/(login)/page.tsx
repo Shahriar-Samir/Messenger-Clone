6be8bad233d63d4eb283101326e117ab3c3c89a5
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-29 01:24:14 +0600

    add: login page title

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -2,14 +2,18 @@ import Image from "next/image";
 
 const page = () => {
   return (
-    <main className="flex justify-center items-center h-[80vh]">
+    <main className="flex justify-center items-center h-[80vh] flex-col">
           <Image 
           height={500} 
           width={500} 
           alt="messenger-icon" 
-          className="w-[50px] h-[50px]" 
+          className="
+          w-[50px] 
+          h-[50px]
+          " 
           src="/icons/messenger.png"
           />
+          <h1 className="mt-5 text-2xl font-extrabold">Sign in to your account</h1>
     </main>
   )
   
