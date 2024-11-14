1781fcaf12a4b97fe155443425255d374c801885
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-14 10:35:45 +0600

    add: login page design

--- components/login/SigninForm.tsx
+++ components/login/SigninForm.tsx
@@ -2,10 +2,19 @@
 
 const SigninForm = () => {
     return (
-        <form className='flex flex-col items-center gap-5 mt-10 p-5 bg-white'>
-            <input type='email'/>
-            <input type='password'/>
-            <button>Sign In</button>
+        <form className='flex flex-col items-center gap-5 mt-10 p-5 bg-white w-11/12 max-w-[380px]'>
+
+
+            <section className="flex flex-col w-full">
+            <label htmlFor="email" className="text-sm font-semibold">Email address</label>
+            <input className="mt-2 w-full rounded-lg py-1 px-2 border" type='email'/>
+            </section>
+
+            <section className="flex flex-col w-full">
+            <label htmlFor="email" className="text-sm font-semibold">Password</label>
+            <input className="mt-2 w-full rounded-lg py-1 px-2 border" type='password'/>
+            </section>
+            <button className="w-full bg-[#3775fd] py-2 rounded-lg text-white">Sign In</button>
         </form>
     );
 };
